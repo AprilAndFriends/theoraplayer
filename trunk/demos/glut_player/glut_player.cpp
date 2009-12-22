e13b79671b75274706add0fa8b6487380e10a4bd
kspes <kspes@82d51e09-41c5-afe0-6a62-a505b9d9c8be>
2009-12-22 11:35:08 +0000

    * basic player demo code * moved TheoraExport.h to include/

--- trunk/demos/glut_player/glut_player.cpp
+++ trunk/demos/glut_player/glut_player.cpp
@@ -18,3 +18,92 @@ this program; if not, write to the Free Software Foundation, Inc., 59 Temple
 Place - Suite 330, Boston, MA 02111-1307, USA, or go to
 http://www.gnu.org/copyleft/lesser.txt.
 *************************************************************************************/
+#include <windows.h> // todo: make platform independent
+#include <stdio.h>
+#include <gl\gl.h>
+#include <gl\glu.h>
+#include <gl\glut.h>
+#include "TheoraVideoManager.h"
+
+unsigned int tex_id;
+TheoraVideoManager* mgr;
+
+void display()
+{
+	glClear(GL_COLOR_BUFFER_BIT);
+	glBindTexture(GL_TEXTURE_2D,tex_id);
+
+	glBegin (GL_QUADS);
+	glTexCoord2f(0.0f, 0.0f); glVertex3f(-1.0f, -1.0f,  0.0f);
+	glTexCoord2f(1.0f, 0.0f); glVertex3f( 1.0f, -1.0f,  0.0f);
+	glTexCoord2f(1.0f, 1.0f); glVertex3f( 1.0f,  1.0f,  0.0f);
+	glTexCoord2f(0.0f, 1.0f); glVertex3f(-1.0f,  1.0f,  0.0f);
+	glEnd();
+
+	glutSwapBuffers();
+}
+
+void init()
+{
+	glShadeModel(GL_SMOOTH);
+	glClearColor(0.0f, 0.0f, 0.0f, 0.5f);
+	glEnable(GL_TEXTURE_2D);
+	glPixelStorei(GL_UNPACK_ALIGNMENT,1);
+	glGenTextures(1,&tex_id);
+
+	glBindTexture(GL_TEXTURE_2D,tex_id);
+	unsigned char* b=new unsigned char[512*512*4];
+	memset(b,127,512*512*4);
+	glTexImage2D(GL_TEXTURE_2D,0,3,512,512,0,GL_RGB,GL_UNSIGNED_BYTE,b);
+	glTexParameteri(GL_TEXTURE_2D,GL_TEXTURE_MIN_FILTER,GL_LINEAR);
+	glTexParameteri(GL_TEXTURE_2D,GL_TEXTURE_MAG_FILTER,GL_LINEAR);
+
+	mgr=new TheoraVideoManager();
+}
+
+void destroy()
+{
+	delete mgr;
+}
+
+void reshape(int w,int h)
+{
+	glMatrixMode(GL_PROJECTION);
+	glLoadIdentity();
+    glViewport(0, 0, w, h);
+
+	gluOrtho2D(-1.1f,1.1f,1.1f,-1.1f);
+	glMatrixMode(GL_MODELVIEW);
+	glLoadIdentity();
+}
+
+
+
+void keyboard(unsigned char key,int x,int y)
+{
+  if (key == 27) // esc
+      throw "Exit Requested";
+}
+
+
+void main(int argc,char** argv)
+{
+  glutInit(&argc, argv);
+  glutInitDisplayMode( GLUT_DOUBLE|GLUT_RGBA);
+  //glutInitWindowPosition(0,0);
+  glutInitWindowSize(800,600);
+  glutCreateWindow("glut_player - libtheoraplayer demo");
+  init();
+  glutDisplayFunc(display);
+  glutReshapeFunc(reshape);
+  glutKeyboardFunc(keyboard);
+  glutIdleFunc(display);
+  try { glutMainLoop(); }
+  catch (void*) {}
+
+  destroy();
+}
+
+
+
+
