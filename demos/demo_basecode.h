f53c0f7486b5241e46b2adb9f360036cb51c225c
kspes <kspes@82d51e09-41c5-afe0-6a62-a505b9d9c8be>
2009-12-23 11:12:57 +0000

    moved the generic demo code to demo_codebase.h to simplify demo development

---
+++ demos/demo_basecode.h
@@ -0,0 +1,130 @@
+/************************************************************************************
+This source file is part of the Theora Video Playback Library
+For latest info, see http://libtheoraplayer.sourceforge.net/
+*************************************************************************************
+Copyright (c) 2008-2009 Kresimir Spes (kreso@cateia.com)
+
+This program is free software; you can redistribute it and/or modify it under
+the terms of the GNU Lesser General Public License (LGPL) as published by the 
+Free Software Foundation; either version 2 of the License, or (at your option) 
+any later version.
+
+This program is distributed in the hope that it will be useful, but WITHOUT
+ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more details.
+
+You should have received a copy of the GNU Lesser General Public License along with
+this program; if not, write to the Free Software Foundation, Inc., 59 Temple
+Place - Suite 330, Boston, MA 02111-1307, USA, or go to
+http://www.gnu.org/copyleft/lesser.txt.
+*************************************************************************************/
+#include <windows.h> // todo: make platform independent
+#include <stdio.h>
+#include <gl\gl.h>
+#include <gl\glu.h>
+#include <gl\glut.h>
+#include <string>
+extern std::string window_name;
+
+void init();
+void destroy();
+void update(float);
+void draw();
+void setDebugTitle(char* out);
+
+
+int nextPow2(int x)
+{
+	int y;
+	for (y=1;y<x;y*=2);
+	return y;
+}
+
+unsigned int createTexture(int w,int h)
+{
+	unsigned int tex_id;
+	glGenTextures(1,&tex_id);
+	glBindTexture(GL_TEXTURE_2D,tex_id);
+	unsigned char* b=new unsigned char[w*h*3];
+	memset(b,0,w*h*3);
+
+	glTexImage2D(GL_TEXTURE_2D,0,3,w,h,0,GL_RGB,GL_UNSIGNED_BYTE,b);
+	glTexParameteri(GL_TEXTURE_2D,GL_TEXTURE_MIN_FILTER,GL_LINEAR);
+	glTexParameteri(GL_TEXTURE_2D,GL_TEXTURE_MAG_FILTER,GL_LINEAR);
+	delete b;
+	return tex_id;
+}
+
+void display()
+{
+	glClear(GL_COLOR_BUFFER_BIT);
+
+	draw();
+
+	glutSwapBuffers();
+
+	static unsigned long time=GetTickCount();
+	unsigned long t=GetTickCount();
+
+	update((t-time)/1000.0f);
+
+	static unsigned long fps_timer=time,fps_counter=0;
+	if (t-fps_timer >= 1000)
+	{
+		char title[512],debug[256]="";
+		
+		setDebugTitle(debug);
+		sprintf(title,"%s: %d FPS; %s",window_name.c_str(),fps_counter,debug);
+		glutSetWindowTitle(title);
+		fps_counter=0;
+		fps_timer=t;
+	}
+	else fps_counter++;
+
+	time=t;
+
+}
+
+void reshape(int w,int h)
+{
+	glMatrixMode(GL_PROJECTION);
+	glLoadIdentity();
+    glViewport(0, 0, w, h);
+
+	gluOrtho2D(-1,1,1,-1);
+	glMatrixMode(GL_MODELVIEW);
+	glLoadIdentity();
+}
+
+void keyboard(unsigned char key,int x,int y)
+{
+  if (key == 27) // esc
+      throw "Exit Requested";
+}
+
+
+void main(int argc,char** argv)
+{
+	glutInit(&argc, argv);
+	glutInitDisplayMode( GLUT_DOUBLE|GLUT_RGBA);
+	//glutInitWindowPosition(0,0);
+	glutInitWindowSize(800,600);
+	glutCreateWindow(window_name.c_str());
+	glShadeModel(GL_SMOOTH);
+	glClearColor(0.0f, 0.0f, 0.0f, 0.5f);
+	glEnable(GL_TEXTURE_2D);
+	glPixelStorei(GL_UNPACK_ALIGNMENT,1);
+	init();
+	glutDisplayFunc(display);
+	glutReshapeFunc(reshape);
+	glutKeyboardFunc(keyboard);
+	glutIdleFunc(display);
+	try { glutMainLoop(); }
+	catch (void*) {}
+
+	destroy();
+}
+
+
+
+
--- demos/glut_player/glut_player.cpp
+++ demos/glut_player/glut_player.cpp
@@ -18,27 +18,16 @@ this program; if not, write to the Free Software Foundation, Inc., 59 Temple
 Place - Suite 330, Boston, MA 02111-1307, USA, or go to
 http://www.gnu.org/copyleft/lesser.txt.
 *************************************************************************************/
-#include <windows.h> // todo: make platform independent
-#include <stdio.h>
-#include <gl\gl.h>
-#include <gl\glu.h>
-#include <gl\glut.h>
+#include "../demo_basecode.h"
 #include "TheoraPlayer.h"
 
 unsigned int tex_id;
 TheoraVideoManager* mgr;
 TheoraVideoClip* clip;
+std::string window_name="glut_player";
 
-int nextPow2(int x)
+void draw()
 {
-	int y;
-	for (y=1;y<x;y*=2);
-	return y;
-}
-
-void display()
-{
-	glClear(GL_COLOR_BUFFER_BIT);
 	glBindTexture(GL_TEXTURE_2D,tex_id);
 
 	TheoraVideoFrame* f=clip->getNextFrame();
@@ -57,93 +46,26 @@ void display()
 	glTexCoord2f(w/tw, h/th); glVertex3f( 1.0f,  1.0f,  0.0f);
 	glTexCoord2f(0.0f, h/th); glVertex3f(-1.0f,  1.0f,  0.0f);
 	glEnd();
+}
 
-	glutSwapBuffers();
+void update(float time_increase)
+{
+	mgr->update(time_increase);
+}
 
-	static unsigned long time=GetTickCount();
-	unsigned long t=GetTickCount();
-
-	mgr->update((t-time)/1000.0f);
-
-	static unsigned long fps_timer=time,fps_counter=0;
-	if (t-fps_timer >= 1000)
-	{
-		char title[256];
-		sprintf(title,"glut_player: %d FPS, %d precached frames",fps_counter,clip->getNumPrecachedFrames());
-		glutSetWindowTitle(title);
-		fps_counter=0;
-		fps_timer=t;
-	}
-	else fps_counter++;
-
-	time=t;
-
+void setDebugTitle(char* out)
+{
+	sprintf(out,"%d precached frames",clip->getNumPrecachedFrames());
 }
 
 void init()
 {
-	glShadeModel(GL_SMOOTH);
-	glClearColor(0.0f, 0.0f, 0.0f, 0.5f);
-	glEnable(GL_TEXTURE_2D);
-	glPixelStorei(GL_UNPACK_ALIGNMENT,1);
-
 	mgr=new TheoraVideoManager();
 	clip=mgr->createVideoClip("../media/konqi.ogg");
-
-	glGenTextures(1,&tex_id);
-	glBindTexture(GL_TEXTURE_2D,tex_id);
-	int w=nextPow2(clip->getWidth()),h=nextPow2(clip->getHeight());
-	unsigned char* b=new unsigned char[w*h*3];
-	memset(b,0,w*h*3);
-
-	glTexImage2D(GL_TEXTURE_2D,0,3,w,h,0,GL_RGB,GL_UNSIGNED_BYTE,b);
-	glTexParameteri(GL_TEXTURE_2D,GL_TEXTURE_MIN_FILTER,GL_LINEAR);
-	glTexParameteri(GL_TEXTURE_2D,GL_TEXTURE_MAG_FILTER,GL_LINEAR);
-
-
+	tex_id=createTexture(nextPow2(clip->getWidth()),nextPow2(clip->getHeight()));
 }
 
 void destroy()
 {
 	delete mgr;
 }
-
-void reshape(int w,int h)
-{
-	glMatrixMode(GL_PROJECTION);
-	glLoadIdentity();
-    glViewport(0, 0, w, h);
-
-	gluOrtho2D(-1,1,1,-1);
-	glMatrixMode(GL_MODELVIEW);
-	glLoadIdentity();
-}
-
-void keyboard(unsigned char key,int x,int y)
-{
-  if (key == 27) // esc
-      throw "Exit Requested";
-}
-
-
-void main(int argc,char** argv)
-{
-  glutInit(&argc, argv);
-  glutInitDisplayMode( GLUT_DOUBLE|GLUT_RGBA);
-  //glutInitWindowPosition(0,0);
-  glutInitWindowSize(800,600);
-  glutCreateWindow("glut_player");
-  init();
-  glutDisplayFunc(display);
-  glutReshapeFunc(reshape);
-  glutKeyboardFunc(keyboard);
-  glutIdleFunc(display);
-  try { glutMainLoop(); }
-  catch (void*) {}
-
-  destroy();
-}
-
-
-
-
