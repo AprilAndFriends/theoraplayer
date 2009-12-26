9ae03f97b849feaffec5853bf50975d171f34786
kspes <kspes@82d51e09-41c5-afe0-6a62-a505b9d9c8be>
2009-12-26 22:57:40 +0000

    * av_player beginnings * OpenAL_AudioInterface ported from ogrevideo project

--- demos/av_player/av_player.cpp
+++ demos/av_player/av_player.cpp
@@ -20,12 +20,15 @@ http://www.gnu.org/copyleft/lesser.txt.
 *************************************************************************************/
 #include "../demo_basecode.h"
 #include "TheoraPlayer.h"
+#include "../OpenAL_AudioInterface.h"
 
 unsigned int tex_id;
 TheoraVideoManager* mgr;
 TheoraVideoClip* clip;
 std::string window_name="av_player";
+bool started=1;
 int window_w=800,window_h=600;
+OpenAL_AudioInterfaceFactory* iface_factory;
 
 void draw()
 {
@@ -34,23 +37,34 @@ void draw()
 	TheoraVideoFrame* f=clip->getNextFrame();
 	if (f)
 	{
-		glTexSubImage2D(GL_TEXTURE_2D,0,0,0,f->getWidth(),f->getHeight(),GL_RGB,GL_UNSIGNED_BYTE,f->getBuffer());
+		glTexSubImage2D(GL_TEXTURE_2D,0,0,0,clip->getWidth(),f->getHeight(),GL_RGB,GL_UNSIGNED_BYTE,f->getBuffer());
 		clip->popFrame();
 	}
 
-	glBegin (GL_QUADS);
+	
 	float w=clip->getWidth(),h=clip->getHeight();
 	float tw=nextPow2(w),th=nextPow2(h);
+	
+	glEnable(GL_TEXTURE_2D);
+	drawTexturedQuad(0,0,800,600,w/tw,h/th);
 
-	glTexCoord2f(0.0f, 0.0f); glVertex3f(-1.0f, -1.0f,  0.0f);
-	glTexCoord2f(w/tw, 0.0f); glVertex3f( 1.0f, -1.0f,  0.0f);
-	glTexCoord2f(w/tw, h/th); glVertex3f( 1.0f,  1.0f,  0.0f);
-	glTexCoord2f(0.0f, h/th); glVertex3f(-1.0f,  1.0f,  0.0f);
-	glEnd();
+	glDisable(GL_TEXTURE_2D);
+	drawColoredQuad(0,570,800,30,0,0,0,1);
+	drawWiredQuad(0,570,800,30,1,1,1,1);
+
+	float x=clip->getTimePosition()/clip->getDuration();
+	drawColoredQuad(3,573,794*x,24,1,1,1,1);
 }
 
 void update(float time_increase)
 {
+	if (started)
+	{
+		// let's wait until the system caches up a few frames on startup
+		if (clip->getNumReadyFrames() < clip->getNumPrecachedFrames()*0.5f)
+			return;
+		started=0;
+	}
 	mgr->update(time_increase);
 }
 
@@ -61,17 +75,31 @@ void OnKeyPress(int key)
 
 void setDebugTitle(char* out)
 {
-	sprintf(out,"%d precached frames",clip->getNumPrecachedFrames());
+	int nDropped=clip->getNumDroppedFrames(),nDisplayed=clip->getNumDisplayedFrames();
+	float percent=100*((float) nDropped/nDisplayed);
+	sprintf(out," (%dx%d) %d precached, %d displayed, %d dropped (%.1f %%)",clip->getWidth(),
+		                                                                    clip->getHeight(),
+		                                                                    clip->getNumReadyFrames(),
+		                                                                    nDisplayed,
+		                                                                    nDropped,
+			                                                                percent);
 }
 
 void init()
 {
 	mgr=new TheoraVideoManager();
-	clip=mgr->createVideoClip("../media/bunny.ogg");
+	iface_factory=new OpenAL_AudioInterfaceFactory();
+	mgr->setAudioInterfaceFactory(iface_factory);
+	clip=mgr->createVideoClip("../media/short.ogg",TH_RGB,32);
+//  use this if you want to preload the file into ram and stream from there
+//	clip=mgr->createVideoClip(new TheoraMemoryFileDataSource("../media/short.ogg"),TH_RGB);
+	clip->setAutoRestart(1);
+
 	tex_id=createTexture(nextPow2(clip->getWidth()),nextPow2(clip->getHeight()));
 }
 
 void destroy()
 {
 	delete mgr;
+	delete iface_factory;
 }
