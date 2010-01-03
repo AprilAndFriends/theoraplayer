a8ac6534a36e8b66cb096c9d90d8031a5c0e3356
kspes <kspes@82d51e09-41c5-afe0-6a62-a505b9d9c8be>
2010-01-03 21:18:12 +0000

    1.0 RC1 Tagged

---
+++ tags/1.0RC1/demos/av_player/av_player.cpp
@@ -0,0 +1,121 @@
+/************************************************************************************
+This source file is part of the Theora Video Playback Library
+For latest info, see http://libtheoraplayer.sourceforge.net/
+*************************************************************************************
+Copyright (c) 2008-2009 Kresimir Spes (kreso@cateia.com)
+
+This program is free software; you can redistribute it and/or modify it under
+the terms of the GNU Lesser General Public License (LGPL) as published by the 
+Free Software Foundation; either version 2 of the License, or (at your option) 
+any later version.
+
+This program is distributed in the hope that it will be useful, but WITHOUT
+ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more details.
+
+You should have received a copy of the GNU Lesser General Public License along with
+this program; if not, write to the Free Software Foundation, Inc., 59 Temple
+Place - Suite 330, Boston, MA 02111-1307, USA, or go to
+http://www.gnu.org/copyleft/lesser.txt.
+*************************************************************************************/
+#include "../demo_basecode.h"
+#include "TheoraPlayer.h"
+#include "../OpenAL_AudioInterface.h"
+
+unsigned int tex_id;
+TheoraVideoManager* mgr;
+TheoraVideoClip* clip;
+std::string window_name="av_player";
+bool started=1;
+int window_w=800,window_h=600;
+OpenAL_AudioInterfaceFactory* iface_factory;
+
+void draw()
+{
+	glBindTexture(GL_TEXTURE_2D,tex_id);
+
+	TheoraVideoFrame* f=clip->getNextFrame();
+
+	if (f)
+	{
+		glTexSubImage2D(GL_TEXTURE_2D,0,0,0,clip->getWidth(),f->getHeight(),GL_RGB,GL_UNSIGNED_BYTE,f->getBuffer());
+		clip->popFrame();
+	}
+
+	
+	float w=clip->getWidth(),h=clip->getHeight();
+	float tw=nextPow2(w),th=nextPow2(h);
+	
+	glEnable(GL_TEXTURE_2D);
+	if (shader_on) enable_shader();
+	drawTexturedQuad(0,0,800,600,w/tw,h/th);
+	if (shader_on) disable_shader();
+
+	glDisable(GL_TEXTURE_2D);
+	drawColoredQuad(0,570,800,30,0,0,0,1);
+	drawWiredQuad(0,570,800,30,1,1,1,1);
+
+	float x=clip->getTimePosition()/clip->getDuration();
+	drawColoredQuad(3,573,794*x,24,1,1,1,1);
+}
+
+void update(float time_increase)
+{
+	if (started)
+	{
+		// let's wait until the system caches up a few frames on startup
+		if (clip->getNumReadyFrames() < clip->getNumPrecachedFrames()*0.5f)
+			return;
+		started=0;
+	}
+	mgr->update(time_increase);
+}
+
+void OnKeyPress(int key)
+{
+	if (key == ' ')
+		if (clip->isPaused()) clip->play(); else clip->pause();
+
+	if (key == 5) clip->setOutputMode(TH_RGB);
+	if (key == 6) clip->setOutputMode(TH_YUV);
+	if (key == 7) clip->setOutputMode(TH_GREY3);
+}
+
+void OnClick(float x,float y)
+{
+	if (y > 570)
+		clip->seek((x/window_w)*clip->getDuration());
+}
+
+void setDebugTitle(char* out)
+{
+	int nDropped=clip->getNumDroppedFrames(),nDisplayed=clip->getNumDisplayedFrames();
+	float percent=100*((float) nDropped/nDisplayed);
+	sprintf(out," (%dx%d) %d precached, %d displayed, %d dropped (%.1f %%)",clip->getWidth(),
+		                                                                    clip->getHeight(),
+		                                                                    clip->getNumReadyFrames(),
+		                                                                    nDisplayed,
+		                                                                    nDropped,
+			                                                                percent);
+}
+
+void init()
+{
+	
+
+	mgr=new TheoraVideoManager();
+	iface_factory=new OpenAL_AudioInterfaceFactory();
+	mgr->setAudioInterfaceFactory(iface_factory);
+	clip=mgr->createVideoClip("../media/bunny.ogg");
+//  use this if you want to preload the file into ram and stream from there
+//	clip=mgr->createVideoClip(new TheoraMemoryFileDataSource("../media/short.ogg"),TH_RGB);
+	clip->setAutoRestart(1);
+
+	tex_id=createTexture(nextPow2(clip->getWidth()),nextPow2(clip->getHeight()));
+}
+
+void destroy()
+{
+	delete mgr;
+	delete iface_factory;
+}
---
+++ tags/1.0RC1/scripts/doxygen_mainpage.h
@@ -0,0 +1,19 @@
+
+/** @mainpage Theora Playback Library
+*
+* @authors kspes
+* <img src="https://sourceforge.net/apps/mediawiki/libtheoraplayer/nfs/project/l/li/libtheoraplayer/7/70/MediaWikiSidebarLogo.png" />
+* @section intro Introduction
+* This is the API reference page for the library.
+* Consult the <a href="https://sourceforge.net/apps/mediawiki/libtheoraplayer/index.php?title=Main_Page">Project Wiki</a> for tutorials and usage.
+* 
+* <hr>
+* @section notes Release Notes
+* This is the 1.0 Release Candidate 1, please test the library as thorougly as possible.
+* 1.0 goals can be seen on the <a href="https://sourceforge.net/apps/mediawiki/libtheoraplayer/index.php?title=Roadmap">Roadmap</a> section of the Project Wiki.
+* <hr>
+* @section requirements Requirements
+* -# libogg
+* -# libvorbis
+* -# libtheora
+*/
