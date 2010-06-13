a012adc3263ca02a5f93a0ca3b9210f94c6e5f80
kspes <kspes@82d51e09-41c5-afe0-6a62-a505b9d9c8be>
2010-06-13 11:05:15 +0000

    License changed from LGPL to BSD

--- include/TheoraAudioInterface.h
+++ include/TheoraAudioInterface.h
@@ -3,20 +3,8 @@ This source file is part of the Theora Video Playback Library
 For latest info, see http://libtheoraplayer.sourceforge.net/
 *************************************************************************************
 Copyright (c) 2008-2010 Kresimir Spes (kreso@cateia.com)
-
 This program is free software; you can redistribute it and/or modify it under
-the terms of the GNU Lesser General Public License (LGPL) as published by the
-Free Software Foundation; either version 2 of the License, or (at your option)
-any later version.
-
-This program is distributed in the hope that it will be useful, but WITHOUT
-ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
-FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more details.
-
-You should have received a copy of the GNU Lesser General Public License along with
-this program; if not, write to the Free Software Foundation, Inc., 59 Temple
-Place - Suite 330, Boston, MA 02111-1307, USA, or go to
-http://www.gnu.org/copyleft/lesser.txt.
+the terms of the BSD license: http://www.opensource.org/licenses/bsd-license.php
 *************************************************************************************/
 #ifndef _TheoraAudioInterface_h
 #define _TheoraAudioInterface_h
