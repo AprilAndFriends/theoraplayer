99561e0346d7e417ba8e38bc40b406f5aee8cfd6
kspes <kspes@82d51e09-41c5-afe0-6a62-a505b9d9c8be>
2010-06-13 11:05:15 +0000

    License changed from LGPL to BSD

--- trunk/src/TheoraUtil.cpp
+++ trunk/src/TheoraUtil.cpp
@@ -1,3 +1,11 @@
+/************************************************************************************
+This source file is part of the Theora Video Playback Library
+For latest info, see http://libtheoraplayer.sourceforge.net/
+*************************************************************************************
+Copyright (c) 2008-2010 Kresimir Spes (kreso@cateia.com)
+This program is free software; you can redistribute it and/or modify it under
+the terms of the BSD license: http://www.opensource.org/licenses/bsd-license.php
+*************************************************************************************/
 #include <stdio.h>
 #include <algorithm>
 #include <math.h>
