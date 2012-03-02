a842ede66ba2901176ecd9012e9fc69e608c0881
kspes <kspes@82d51e09-41c5-afe0-6a62-a505b9d9c8be>
2012-03-02 20:06:25 +0000

    * changed copyright headers to reflect current information * added initial seeking test demo code

--- include/theoraplayer/TheoraVideoFrame.h
+++ include/theoraplayer/TheoraVideoFrame.h
@@ -2,7 +2,7 @@
 This source file is part of the Theora Video Playback Library
 For latest info, see http://libtheoraplayer.sourceforge.net/
 *************************************************************************************
-Copyright (c) 2008-2010 Kresimir Spes (kreso@cateia.com)
+Copyright (c) 2008-2012 Kresimir Spes (kspes@cateia.com)
 This program is free software; you can redistribute it and/or modify it under
 the terms of the BSD license: http://www.opensource.org/licenses/bsd-license.php
 *************************************************************************************/
