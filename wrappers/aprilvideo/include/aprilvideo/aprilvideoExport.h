057f89017593dd9092343e73a6d71e36e1fb3372
kspes <kspes@82d51e09-41c5-afe0-6a62-a505b9d9c8be>
2012-09-09 10:01:49 +0000

    initial code for the aprilvideo wrapper

---
+++ wrappers/aprilvideo/include/aprilvideo/aprilvideoExport.h
@@ -0,0 +1,34 @@
+/************************************************************************************
+This source file is part of the Theora Video Playback Library
+For latest info, see http://libtheoraplayer.sourceforge.net/
+*************************************************************************************
+Copyright (c) 2008-2012 Kresimir Spes (kspes@cateia.com)
+This program is free software; you can redistribute it and/or modify it under
+the terms of the BSD license: http://www.opensource.org/licenses/bsd-license.php
+*************************************************************************************/
+#ifndef APRILVIDEO_EXPORT_H
+/************************************************************************************
+This source file is part of the Theora Video Playback Library
+For latest info, see http://libtheoraplayer.sourceforge.net/
+*************************************************************************************
+Copyright (c) 2008-2012 Kresimir Spes (kspes@cateia.com)
+This program is free software; you can redistribute it and/or modify it under
+the terms of the BSD license: http://www.opensource.org/licenses/bsd-license.php
+*************************************************************************************/
+#define APRILVIDEO_EXPORT_H
+
+#ifdef _WIN32
+	#ifdef APRILVIDEO_STATIC
+		#define AprilVideoExport
+	#else
+		#ifdef APRILVIDEO_EXPORTS
+			#define AprilVideoExport __declspec(dllexport)
+		#else
+			#define AprilVideoExport __declspec(dllimport)
+		#endif
+    #endif
+#else
+	#define AprilVideoExport __attribute__ ((visibility("default")))
+#endif
+
+#endif
---
+++ wrappers/aprilvideo/readme.txt
@@ -0,0 +1,15 @@
+This wrapper is used in the "April & Friends" project developed by the author of Theora Playback Library.
+
+To compile and use this, you'll need:
+* gtypes
+* hltypes
+* april
+* atres
+* aprilui
+* ogg
+* vorbis
+* theora
+* theoraplayer
+
+If you have any questions, feel free to use Theora Playback Library's forum
+or contact me directly through Sourceforge.
