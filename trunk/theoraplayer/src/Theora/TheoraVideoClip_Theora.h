5c8b8c2e422a3347200669be49b5c7d4d50a062d
kspes <kspes@82d51e09-41c5-afe0-6a62-a505b9d9c8be>
2013-01-06 01:46:47 +0000

    * iOS demo basecode * split demo_basecode.h source into several cpp files * first working iOS demo

--- trunk/theoraplayer/src/Theora/TheoraVideoClip_Theora.h
+++ trunk/theoraplayer/src/Theora/TheoraVideoClip_Theora.h
@@ -1,11 +1,11 @@
 /************************************************************************************
- This source file is part of the Theora Video Playback Library
- For latest info, see http://libtheoraplayer.sourceforge.net/
- *************************************************************************************
- Copyright (c) 2008-2013 Kresimir Spes (kspes@cateia.com)
- This program is free software; you can redistribute it and/or modify it under
- the terms of the BSD license: http://www.opensource.org/licenses/bsd-license.php
- *************************************************************************************/
+This source file is part of the Theora Video Playback Library
+For latest info, see http://libtheoraplayer.sourceforge.net/
+*************************************************************************************
+Copyright (c) 2008-2013 Kresimir Spes (kspes@cateia.com)
+This program is free software; you can redistribute it and/or modify it under
+the terms of the BSD license: http://www.opensource.org/licenses/bsd-license.php
+*************************************************************************************/
 #if defined(__THEORA) && !defined(_TheoraVideoClip_Theora_h)
 #define _TheoraVideoClip_Theora_h
 
