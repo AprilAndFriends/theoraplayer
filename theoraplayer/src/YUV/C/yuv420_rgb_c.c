2b34d82960c3ece22eb2b1466ad674eb9b744ea2
kspes <kspes@82d51e09-41c5-afe0-6a62-a505b9d9c8be>
2013-01-28 14:52:15 +0000

    * fixed YUV->RGB conversion, correcting my wrong assumption that swapping U & V would correctly swap R & B :)

--- theoraplayer/src/YUV/C/yuv420_rgb_c.c
+++ theoraplayer/src/YUV/C/yuv420_rgb_c.c
@@ -13,108 +13,104 @@ the terms of the BSD license: http://www.opensource.org/licenses/bsd-license.php
 	if ((tmp & ~0xFF) == 0) dst = tmp;\
 	else                    dst = (-tmp) >> 31;
 
-static void _decodeRGB(struct TheoraPixelTransform* t, int stride, int nBytes, int maxWidth)
-{
-	register int tmp;
-	int cv, cu, rgbY1, rgbY2, rgbY3, rgbY4, rV, gUV, bU, y, width = maxWidth == 0 ? t->w : maxWidth;
-	unsigned char *ySrcEven, *ySrcOdd, *yLineEnd, *uSrc, *vSrc, *out1, *out2;
-
-	for (y=0; y < t->h; y += 2)
-	{
-		ySrcEven = t->y + y * t->yStride;
-		ySrcOdd  = t->y + (y + 1) * t->yStride;
-		uSrc = t->u + y * t->uStride / 2;
-		vSrc = t->v + y * t->vStride / 2;
-		out1 = t->out + y * stride;
-		out2 = t->out + (y + 1) * stride;
-		
-		for (yLineEnd = ySrcEven + width; ySrcEven != yLineEnd;)
-		{
-			// EVEN columns
-			cu = *uSrc; uSrc++;
-			cv = *vSrc; vSrc++;
-			rV   = RVTable[cv];
-			gUV  = GUTable[cu] + GVTable[cv];
-			bU   = BUTable[cu];
-			
-			rgbY1 = YTable[*ySrcEven]; ySrcEven++;
-			rgbY2 = YTable[*ySrcOdd];  ySrcOdd++;
-			rgbY3 = YTable[*ySrcEven]; ySrcEven++;
-			rgbY4 = YTable[*ySrcOdd];  ySrcOdd++;
-			
-			// EVEN columns
-			CLIP_RGB_COLOR(out1[0], rgbY1 + rV );
-			CLIP_RGB_COLOR(out1[1], rgbY1 - gUV);
-			CLIP_RGB_COLOR(out1[2], rgbY1 + bU );
-			
-			CLIP_RGB_COLOR(out2[0], rgbY2 + rV );
-			CLIP_RGB_COLOR(out2[1], rgbY2 - gUV);
-			CLIP_RGB_COLOR(out2[2], rgbY2 + bU );
-
-			out1 += nBytes;  out2 += nBytes;
-			// ODD columns
-			CLIP_RGB_COLOR(out1[0], rgbY3 + rV );
-			CLIP_RGB_COLOR(out1[1], rgbY3 - gUV);
-			CLIP_RGB_COLOR(out1[2], rgbY3 + bU );
-			
-			CLIP_RGB_COLOR(out2[0], rgbY4 + rV );
-			CLIP_RGB_COLOR(out2[1], rgbY4 - gUV);
-			CLIP_RGB_COLOR(out2[2], rgbY4 + bU );
-			out1 += nBytes;  out2 += nBytes;
-		}
+#define _decodeRGB(stride, nBytes, maxWidth, i1, i2, i3, j1, j2, j3)\
+	register int tmp;\
+	int nBytes2 = nBytes * 2, cv, cu, rgbY1, rgbY2, rgbY3, rgbY4, rV, gUV, bU, width = maxWidth == 0 ? t->w : maxWidth;\
+	unsigned int y;\
+	unsigned char *ySrcEven, *ySrcOdd, *yLineEnd, *uSrc, *vSrc, *out1, *out2;\
+	\
+	for (y=0; y < t->h; y += 2)\
+	{\
+		ySrcEven = t->y + y * t->yStride;\
+		ySrcOdd  = t->y + (y + 1) * t->yStride;\
+		uSrc = t->u + y * t->uStride / 2;\
+		vSrc = t->v + y * t->vStride / 2;\
+		out1 = t->out + y * stride;\
+		out2 = t->out + (y + 1) * stride;\
+		\
+		for (yLineEnd = ySrcEven + width; ySrcEven != yLineEnd;)\
+		{\
+			cu = *uSrc; uSrc++;\
+			cv = *vSrc; vSrc++;\
+			rV   = RVTable[cv];\
+			gUV  = GUTable[cu] + GVTable[cv];\
+			bU   = BUTable[cu];\
+			\
+			rgbY1 = YTable[*ySrcEven]; ySrcEven++;\
+			rgbY2 = YTable[*ySrcOdd];  ySrcOdd++;\
+			rgbY3 = YTable[*ySrcEven]; ySrcEven++;\
+			rgbY4 = YTable[*ySrcOdd];  ySrcOdd++;\
+			\
+			CLIP_RGB_COLOR(out1[i1], rgbY1 + rV );\
+			CLIP_RGB_COLOR(out1[i2], rgbY1 - gUV);\
+			CLIP_RGB_COLOR(out1[i3], rgbY1 + bU );\
+			\
+			CLIP_RGB_COLOR(out2[i1], rgbY2 + rV );\
+			CLIP_RGB_COLOR(out2[i2], rgbY2 - gUV);\
+			CLIP_RGB_COLOR(out2[i3], rgbY2 + bU );\
+			\
+			CLIP_RGB_COLOR(out1[j1], rgbY3 + rV );\
+			CLIP_RGB_COLOR(out1[j2], rgbY3 - gUV);\
+			CLIP_RGB_COLOR(out1[j3], rgbY3 + bU );\
+			\
+			CLIP_RGB_COLOR(out2[j1], rgbY4 + rV );\
+			CLIP_RGB_COLOR(out2[j2], rgbY4 - gUV);\
+			CLIP_RGB_COLOR(out2[j3], rgbY4 + bU );\
+			\
+			out1 += nBytes2;  out2 += nBytes2;\
+		}\
 	}
-}
 
 void decodeRGB(struct TheoraPixelTransform* t)
 {
-	_decodeRGB(t, t->w * 3, 3, 0);
+	_decodeRGB(t->w * 3, 3, 0, 0, 1, 2, 3, 4, 5);
 }
 
 void decodeRGBA(struct TheoraPixelTransform* t)
 {
-	_decodeRGB(t, t->w * 4, 4, 0);
+	_decodeRGB(t->w * 4, 4, 0, 0, 1, 2, 4, 5, 6);
 	_decodeAlpha(incOut(t, 3), t->w * 4);
 }
 
 void decodeRGBX(struct TheoraPixelTransform* t)
 {
-	_decodeRGB(t, t->w * 4, 4, 0);
+	_decodeRGB(t->w * 4, 4, 0, 0, 1, 2, 4, 5, 6);
 }
 
 void decodeARGB(struct TheoraPixelTransform* t)
 {
-	_decodeRGB(incOut(t, 1), t->w * 4, 4, 0);
+	_decodeRGB(t->w * 4, 4, 0, 1, 2, 3, 5, 6, 7);
 	_decodeAlpha(t, t->w * 4);
 }
 
 void decodeXRGB(struct TheoraPixelTransform* t)
 {
-	_decodeRGB(incOut(t, 1), t->w * 4, 4, 0);
+	_decodeRGB(t->w * 4, 4, 0, 1, 2, 3, 5, 6, 7);
 }
 
 void decodeBGR(struct TheoraPixelTransform* t)
 {
-	_decodeRGB(swapUV(t), t->w * 3, 3, 0);
+	_decodeRGB(t->w * 3, 3, 0, 2, 1, 0, 5, 4, 3);
 }
 
 void decodeBGRA(struct TheoraPixelTransform* t)
 {
-	_decodeRGB(swapUV(t), t->w * 4, 4, 0);
+	_decodeRGB(t->w * 4, 4, 0, 2, 1, 0, 6, 5, 4);
 	_decodeAlpha(incOut(t, 3), t->w * 4);
 }
 
 void decodeBGRX(struct TheoraPixelTransform* t)
 {
-	_decodeRGB(swapUV(t), t->w * 4, 4, 0);
+	_decodeRGB(t->w * 4, 4, 0, 2, 1, 0, 6, 5, 4);
 }
 
 void decodeABGR(struct TheoraPixelTransform* t)
 {
-	_decodeRGB(swapUV(incOut(t, 1)), t->w * 4, 4, 0);
+	_decodeRGB(t->w * 4, 4, 0, 3, 2, 1, 7, 6, 5);
 	_decodeAlpha(t, t->w * 4);
 }
 
 void decodeXBGR(struct TheoraPixelTransform* t)
 {
-	_decodeRGB(swapUV(incOut(t, 1)), t->w * 4, 4, 0);
+	_decodeRGB(t->w * 4, 4, 0, 3, 2, 1, 7, 6, 5);
 }
