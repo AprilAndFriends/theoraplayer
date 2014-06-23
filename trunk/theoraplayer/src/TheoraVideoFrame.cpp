5bf21c24d2235785e49221f844e912fcf0e8a76b
kresimir.spes <kresimir.spes@82d51e09-41c5-afe0-6a62-a505b9d9c8be>
2014-06-23 07:19:36 +0000

    added include memory.h to fix compilation issues on linux

--- trunk/theoraplayer/src/TheoraVideoFrame.cpp
+++ trunk/theoraplayer/src/TheoraVideoFrame.cpp
@@ -6,6 +6,7 @@ Copyright (c) 2008-2014 Kresimir Spes (kspes@cateia.com)
 This program is free software; you can redistribute it and/or modify it under
 the terms of the BSD license: http://opensource.org/licenses/BSD-3-Clause
 *************************************************************************************/
+#include <memory.h>
 #include "TheoraPixelTransform.h"
 #include "TheoraVideoClip.h"
 #include "TheoraVideoFrame.h"
