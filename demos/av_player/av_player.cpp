701ff4e60ab985f84c60e37c486db2cb39278371
kresimir.spes <kresimir.spes@82d51e09-41c5-afe0-6a62-a505b9d9c8be>
2014-10-13 11:01:12 +0000

    restored filename, accidental debug commit

--- demos/av_player/av_player.cpp
+++ demos/av_player/av_player.cpp
@@ -118,7 +118,7 @@ void init()
 	
 	//*/
 	//resourceExtension = ".ogg"; // TEMP
-	clip=mgr->createVideoClip("media/xmen.ogv", outputMode, 16);
+	clip=mgr->createVideoClip("media/bunny" + resourceExtension, outputMode, 16);
 //  use this if you want to preload the file into ram and stream from there
 //	clip=mgr->createVideoClip(new TheoraMemoryFileDataSource("../media/short" + resourceExtension),TH_RGB);
 	clip->setAutoRestart(1);
